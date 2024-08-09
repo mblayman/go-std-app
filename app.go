ac33138ee31791a902ce228359fb48584eb852e7
Matt Layman <matthewlayman@gmail.com>
2024-08-08 23:05:49 -0400

    Covered routing topics.

--- app.go
+++ app.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("Starting server...")
+
+	router := http.NewServeMux()
+	router.HandleFunc("/users/{user}", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello %s", html.EscapeString(r.PathValue("user")))
+	})
+
+	v2 := http.NewServeMux()
+	v2.HandleFunc("/users/{user}", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello v2 %s", html.EscapeString(r.PathValue("user")))
+	})
+
+	router.Handle("/v2/", http.StripPrefix("/v2", v2))
+
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+	log.Fatal(server.ListenAndServe())
 }
