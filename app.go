11da7fd95048c2aca24a24160550fe203e67068d
Matt Layman <matthewlayman@gmail.com>
2024-08-28 00:22:31 -0400

    Add JSON example.

--- app.go
+++ app.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html"
 	"net/http"
 )
+
+type Movie struct {
+	Title       string `json:"title"`
+	ReleaseYear int    `json:"releaseYear"`
+}
 
 func main() {
 	fmt.Println("Starting server...")
@@ -22,6 +28,16 @@ func main() {
 	})
 
 	router.Handle("/v2/", http.StripPrefix("/v2", v2))
+
+	// JSON
+	router.HandleFunc("GET /movies", func(w http.ResponseWriter, r *http.Request) {
+		movies := []Movie{
+			{Title: "The Shawshank Redemption", ReleaseYear: 1994},
+			{Title: "The Usual Suspects", ReleaseYear: 1995},
+		}
+		encoder := json.NewEncoder(w)
+		encoder.Encode(movies)
+	})
 
 	server := http.Server{
 		Addr:    ":8000",
