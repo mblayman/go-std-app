7582d2087aa347f04ea47d7af89bf03d77892dd6
Matt Layman <matthewlayman@gmail.com>
2024-08-28 00:16:26 -0400

    Add basic routing.

--- app.go
+++ app.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html"
 	"net/http"
 )
 
@@ -9,6 +10,18 @@ func main() {
 	fmt.Println("Starting server...")
 
 	router := http.NewServeMux()
+
+	// Basic routing
+	router.HandleFunc("/users/{user}", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello %s", html.EscapeString(r.PathValue("user")))
+	})
+
+	v2 := http.NewServeMux()
+	v2.HandleFunc("/users/{user}", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello v2 %s", html.EscapeString(r.PathValue("user")))
+	})
+
+	router.Handle("/v2/", http.StripPrefix("/v2", v2))
 
 	server := http.Server{
 		Addr:    ":8000",
